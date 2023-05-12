0de42347b3e3eed2d8a74fc3877314ffa40abdee
Milena Reis <villelamilena@gmail.com>
2023-05-12 19:43:39 -0300

    more content

--- treat_organiz_ clean.py
+++ treat_organiz_ clean.py
@@ -43,6 +43,7 @@ covid_sp.rename(columns = {"codigo_ibge": "cod_munic",
 
 for column in covid_sp.columns:
     print(column)
+del column
     
 #deleting column by colname
 
@@ -158,3 +159,94 @@ df
 
 del df,x,y,list_y
 
+# deleting rows by row number
+covid_sp_test.head()
+covid_sp_test2 = covid_sp_test.drop(covid_sp_test.index[[1,3]])
+covid_sp_test2.head()
+
+covid_sp_test2 = covid_sp_test2.drop(covid_sp_test2.index[2:4])
+covid_sp_test2.head()
+
+#notice the difference: when you chose row by row, you have to write two [[]],
+#when you chose a sequence/interval you use onlye one[]
+
+#reset index
+covid_sp_test2 = covid_sp_test2.reset_index(drop=True)#drop to delete previous index
+covid_sp_test2.head()
+
+del covid_sp_test2
+
+#data frame with municipio not identified
+ignored = covid_sp_test.loc[covid_sp_test.municipio == "Ignorado"]
+ignored
+
+ignored.shape
+#checking the size of the non-indentified data
+tuple(i/j for i,j in zip(ignored.shape,covid_sp_test.shape))
+
+del ignored
+
+#deleting non-identified data
+covid_sp_test.shape
+covid_sp_test = covid_sp_test.loc[covid_sp_test.municipio != "Ignorado"]
+covid_sp_test.shape
+
+guarulhos = covid_sp_test.loc[covid_sp_test.municipio == "Guarulhos"]
+guarulhos
+
+guarulhos.drop(columns = ["map_leg_s","municipio"],inplace = True)
+
+#changing specific value on dataframe
+guarulhos['semana_epidem'] = guarulhos['semana_epidem'].replace({9: "nove",
+                                                                 10: "dez"})
+guarulhos
+guarulhos.semana_epidem = guarulhos.semana_epidem.replace({38: "trinta e oito"})
+guarulhos
+
+guarulhos['semana_epidem'] = guarulhos['semana_epidem'].replace([11,12,13],
+                                                                ["onze","doze","treze"])
+guarulhos.head(30)
+
+#change , to . in string
+guarulhos.casos_pc
+guarulhos.casos_pc = guarulhos.casos_pc.apply(lambda x: x.replace(',','.'))
+guarulhos.casos_pc
+
+#create columns with data
+import datetime
+
+date = np.array('2020-02-25',dtype = np.datetime64())
+date
+
+date = date + np.arange(579)
+date
+
+date = pd.DataFrame(date) #making it as dataframe
+date
+
+date.columns = ["date"]
+date.head()
+
+guarulhos2 = pd.concat([date,guarulhos],axis = 1)
+guarulhos2.head()
+
+"""Out[52]: 
+        date  index  mes  casos  ...  area map_leg semana_epidem  density
+0 2020-02-25    NaN  NaN    NaN  ...   NaN     NaN           NaN      NaN
+1 2020-02-26    NaN  NaN    NaN  ...   NaN     NaN           NaN      NaN
+2 2020-02-27    NaN  NaN    NaN  ...   NaN     NaN           NaN      NaN
+3 2020-02-28    NaN  NaN    NaN  ...   NaN     NaN           NaN      NaN
+4 2020-02-29    NaN  NaN    NaN  ...   NaN     NaN           NaN      NaN
+"""
+# notice that there is a problem and that is due to the index (date index is different
+# from gurarulhos index)
+
+#so first reset index of guarulhos
+guarulhos = guarulhos.reset_index(drop=True)#drop to delete previous index
+guarulhos.head()
+
+#and then concat
+guarulhos2 = pd.concat([date,guarulhos],axis = 1)
+guarulhos2.head()
+
+del guarulhos, guarulhos2, date
